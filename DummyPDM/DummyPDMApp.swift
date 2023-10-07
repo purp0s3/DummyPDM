b565478786872a417e30d36178f701a4d552be15
Lucas Nascimento <purp0s3@icloud.com>
2023-10-07 09:43:00 -0300

    Initial Commit

---
+++ DummyPDM/DummyPDMApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyPDMApp.swift
+//  DummyPDM
+//
+//  Created by Lucas Nascimento on 07/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DummyPDMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
