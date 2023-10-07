97256d039fc37e5f6960080f4770a8ee63739603
Lucas Nascimento <purp0s3@icloud.com>
2023-10-07 09:51:38 -0300

    create project

--- DummyPDM/ContentView.swift
+++ DummyPDM/ContentView.swift
@@ -9,16 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            SignInView()
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
